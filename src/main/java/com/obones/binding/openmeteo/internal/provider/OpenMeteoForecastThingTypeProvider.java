e472076d07e05b0b270eb6e08108d6de5ced1cb1
OBones <obones@free.fr>
2024-03-02 18:17:01 +0100

    feat: give proper label and description for split channels

--- src/main/java/com/obones/binding/openmeteo/internal/provider/OpenMeteoForecastThingTypeProvider.java
+++ src/main/java/com/obones/binding/openmeteo/internal/provider/OpenMeteoForecastThingTypeProvider.java
@@ -24,6 +24,8 @@ import java.util.Locale;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.i18n.LocaleProvider;
+import org.openhab.core.i18n.TranslationProvider;
 import org.openhab.core.thing.ThingTypeUID;
 import org.openhab.core.thing.binding.ThingTypeProvider;
 import org.openhab.core.thing.i18n.ThingTypeI18nLocalizationService;
@@ -38,6 +40,8 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.obones.binding.openmeteo.internal.utils.Localization;
+
 @NonNullByDefault
 @Component(service = ThingTypeProvider.class)
 public class OpenMeteoForecastThingTypeProvider implements ThingTypeProvider {
@@ -45,12 +49,32 @@ public class OpenMeteoForecastThingTypeProvider implements ThingTypeProvider {
     private final Logger logger = LoggerFactory.getLogger(OpenMeteoForecastThingTypeProvider.class);
     private final ThingTypeI18nLocalizationService localizationService;
     private final Bundle bundle;
+    private @NonNullByDefault({}) LocaleProvider localeProvider;
+    private @NonNullByDefault({}) TranslationProvider i18nProvider;
+    private Localization localization = Localization.UNKNOWN;
 
     @Activate
-    public OpenMeteoForecastThingTypeProvider(final @Reference ThingTypeI18nLocalizationService localizationService,
-            ComponentContext componentContext) {
+    public OpenMeteoForecastThingTypeProvider(final @Reference LocaleProvider givenLocaleProvider,
+            final @Reference TranslationProvider givenI18nProvider,
+            final @Reference ThingTypeI18nLocalizationService localizationService, ComponentContext componentContext) {
         this.bundle = componentContext.getBundleContext().getBundle();
+        localeProvider = givenLocaleProvider;
+        i18nProvider = givenI18nProvider;
         this.localizationService = localizationService;
+    }
+
+    @Reference
+    protected void setLocaleProvider(final LocaleProvider givenLocaleProvider) {
+        logger.trace("setLocaleProvider(): provided locale={}.", givenLocaleProvider);
+        localeProvider = givenLocaleProvider;
+        updateLocalization();
+    }
+
+    @Reference
+    protected void setTranslationProvider(TranslationProvider givenI18nProvider) {
+        logger.trace("setTranslationProvider(): provided translation={}.", givenI18nProvider);
+        i18nProvider = givenI18nProvider;
+        updateLocalization();
     }
 
     @Override
@@ -83,6 +107,14 @@ public class OpenMeteoForecastThingTypeProvider implements ThingTypeProvider {
         return null;
     }
 
+    private void updateLocalization() {
+        if (Localization.UNKNOWN.equals(localization) && (localeProvider != null) && (i18nProvider != null)) {
+            logger.trace("updateLocalization(): creating Localization based on locale={},translation={}).",
+                    localeProvider, i18nProvider);
+            localization = new Localization(localeProvider, i18nProvider);
+        }
+    }
+
     private List<ChannelGroupDefinition> getChannelGroupDefinitions() {
         ArrayList<ChannelGroupDefinition> result = new ArrayList<ChannelGroupDefinition>();
 
@@ -92,20 +124,32 @@ public class OpenMeteoForecastThingTypeProvider implements ThingTypeProvider {
         result.add(new ChannelGroupDefinition(CHANNEL_GROUP_HOURLY_TIME_SERIES, CHANNEL_GROUP_TYPE_HOURLY_TIME_SERIES));
 
         DecimalFormat hourlyFormatter = new DecimalFormat("00");
-        for (int hour = 1; hour < 24 * maximumDays; hour++) {
+        String hourlyLabelFormat = localization.getText("channel-group-type.openmeteo.forecast.hourly.label.format");
+        String hourlyDescriptionFormat = localization
+                .getText("channel-group-type.openmeteo.forecast.hourly.description.format");
+        for (int hour = 1; hour <= 24 * maximumDays; hour++) {
             result.add(new ChannelGroupDefinition(CHANNEL_GROUP_HOURLY_PREFIX + hourlyFormatter.format(hour),
-                    CHANNEL_GROUP_TYPE_HOURLY));
+                    CHANNEL_GROUP_TYPE_HOURLY, String.format(hourlyLabelFormat, hour),
+                    String.format(hourlyDescriptionFormat, hour)));
         }
 
         // daily forecast
         result.add(new ChannelGroupDefinition(CHANNEL_GROUP_DAILY_TIME_SERIES, CHANNEL_GROUP_TYPE_DAILY_TIME_SERIES));
 
+        result.add(new ChannelGroupDefinition(CHANNEL_GROUP_DAILY_TODAY, CHANNEL_GROUP_TYPE_DAILY,
+                localization.getText("channel-group-type.openmeteo.forecast.daily.today.label"),
+                localization.getText("channel-group-type.openmeteo.forecast.daily.today.description")));
+        result.add(new ChannelGroupDefinition(CHANNEL_GROUP_DAILY_TOMORROW, CHANNEL_GROUP_TYPE_DAILY,
+                localization.getText("channel-group-type.openmeteo.forecast.daily.tomorrow.label"),
+                localization.getText("channel-group-type.openmeteo.forecast.daily.tomorrow.description")));
         DecimalFormat dailyFormatter = new DecimalFormat("00");
-        result.add(new ChannelGroupDefinition(CHANNEL_GROUP_DAILY_TODAY, CHANNEL_GROUP_TYPE_DAILY));
-        result.add(new ChannelGroupDefinition(CHANNEL_GROUP_DAILY_TOMORROW, CHANNEL_GROUP_TYPE_DAILY));
+        String dailyLabelFormat = localization.getText("channel-group-type.openmeteo.forecast.daily.label.format");
+        String dailyDescriptionFormat = localization
+                .getText("channel-group-type.openmeteo.forecast.daily.description.format");
         for (int day = 2; day < maximumDays; day++) {
             result.add(new ChannelGroupDefinition(CHANNEL_GROUP_DAILY_PREFIX + dailyFormatter.format(day),
-                    CHANNEL_GROUP_TYPE_DAILY));
+                    CHANNEL_GROUP_TYPE_DAILY, String.format(dailyLabelFormat, day),
+                    String.format(dailyDescriptionFormat, day)));
         }
 
         return result;
